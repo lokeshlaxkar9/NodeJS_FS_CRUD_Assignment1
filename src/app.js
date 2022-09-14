22a3babcfea6a59b75ed7d2c78474e5c2f727b64
lokeshlaxkar9 <lkshlaxkar@gmail.com>
2022-09-14 20:42:42 +0530

    assignment1 added

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const {
+  myFileWriter,
+  myFileUpdater,
+  myFileReader,
+  myFileDeleter,
+} = require("./index");
+
+// myFileWriter("test.txt", "Hello");
+
+// myFileUpdater("test.txt", " GOOD");
+
+// myFileReader("test.txt");
+
+myFileDeleter("test.txt");
